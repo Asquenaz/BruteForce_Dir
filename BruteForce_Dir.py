1b5847f8d86d6f8158e61f426322a256ceae9f45
FernandesGuilherme <guilherme.logus@gmail.com>
2024-02-17 15:13:16 -0300

    Brute force v1.0.0.3

--- BruteForce_Dir.py
+++ BruteForce_Dir.py
@@ -1,60 +1,99 @@
-#Utilize  python 3 para rodar o script
-#Certifique-se de que a wordlist a ser usada está na mesma pasta do projeto.
+# Utilize  python >=3 para rodar o script
+
+# Certifique-se de que a wordlist a ser usada esta na mesma pasta do projeto.
+# Certifique-se de que todos os arquivos estão no formato UTF 8
 
 #Lembre-se : 
       #Com grandes poderes, vem sempre grandes responsabilidades.
-      #Contríbua com o mundo da cibersegurança.
-
-#Ass: Asquen@az 
-
+      #Contribua com o mundo da ciberseguranca.
 
 import requests
 import sys 
 
+#URLs validas
+path_saida = r"C:\Users\GuilhermeFernandesLi\OneDrive - Bricon IT Solution\Documentos\Bricon\Scripts\BruteForce_Dir/200.txt"
 
-#Lista de diretórios que serão testadas
-#URLs válidas
-path_saida = r"/home/kali/Documents/Scripts/BruteForce/200.txt"
+#Caminho para wordlist padrao
+path_word_default = r"C:\Users\GuilhermeFernandesLi\OneDrive - Bricon IT Solution\Documentos\Bricon\Scripts\BruteForce_Dir/small.txt"
 
-#Urls inválidas
-path_URL_testadas = r"/home/kali/Documents/Scripts/BruteForce/urls_testadas.txt"
+
+path_URL_testadas = r"C:\Users\GuilhermeFernandesLi\OneDrive - Bricon IT Solution\Documentos\Bricon\Scripts\BruteForce_Dir/urls_testadas.txt"
+path_URL_Fail = r"C:\Users\GuilhermeFernandesLi\OneDrive - Bricon IT Solution\Documentos\Bricon\Scripts\BruteForce_Dir/fail.txt"
 
 
 def save_output(path_saida, site, mensagem = None):
-    with open (path_saida, 'a') as saida:
-        saida.write(f"{mensagem} {site}\n")
-        saida.close()
+    with open (path_saida, 'a', encoding='utf-8') as saida:
+        saida.write(f" {site}\n")
+        
+
+def status_code (site) :
+    response = requests.get(site)
+    
+    if response.status_code == 200 :
+        save_output(path_saida, site)
+    elif response.status_code >= 400 and response.status_code <=599 :
+        save_output (path_URL_Fail, site)
+    else :
+        save_output (path_URL_testadas, site)
+  
+    return response.status_code
 
 
-def find_dir(URL, path_entrada):
-
-    with open (path_entrada, 'r') as wordlist:
+def find_dir (URL, list):
+    with open (list, 'r', encoding='utf-8') as wordlist:
         for diretorio in wordlist :
             diretorio = diretorio.strip()
             site = f"http://{URL}/{diretorio}"
 
             try:
-                response = requests.get(site)
-                print(f'Testando: {site} (CODE: {response.status_code})', end='\r')
-
-                if response.status_code == 200 :
-                    save_output(path_saida, site, mensagem="URL válida: ")
-                if response.status_code <=399 and response.status_code >= 599 :
-                    save_output (path_URL_testadas, site, mensagem="Urls que não retornaram erro de sevidor ou usuário : " )
+                status_code(site)
+                print(f'+  {site} (CODE: {status_code(site)}) ', end='\r')
 
             except ConnectionError as ce:
-                print(f"Erro de conexão: {ce}")
+                print(f"Erro de conexao: {ce}")
             except KeyboardInterrupt as e:
-                sys.exit(0)
                 print(f"Erro: {e}")
+                sys.exit(0)
+   # next_brute()
 
-    print('Busca finalizada, para mais detalhes, abra o arquivo 200.txt')
+def find_subDir (wordlist, wordlist_sub):
+    with open(wordlist_sub, "r", encoding='utf-8') as word_sub:
+        for site in word_sub:
+            site = site.strip()
+            
+            with open(wordlist, "r", encoding='utf-8') as wordlist_file:
+                for url in wordlist_file:
+                    url = url.strip()
+                    sub_dir = "{}/{}".format(url, site)
+
+                    try:
+                         status_code(sub_dir)
+                         print(f'+  {sub_dir} (CODE: {status_code(sub_dir)}) ', end='\r')
+
+                    except ConnectionError as ce:
+                        print(f"Erro de conexao: {ce}")
+                    except KeyboardInterrupt as e:
+                        print(f"Erro: {e}")
+                        sys.exit(0)
+    print('Programa encerrado, seus resultados postivos estao no arquivo 200.txt')
+    
+def next_brute():
+    print('')
+    resposta = input(str('Deseja testar subdiretorios : (S/N): ')).lower()
+    if resposta == 's' : 
+        find_subDir(path_saida, path_word_default )
+    else :
+        print('Programa encerrado, seus resultados postivos estao no arquivo 200.txt')
+        sys.exit(0)
 
 if __name__ == '__main__':
     url = sys.argv[1]
     wordlist = sys.argv[2]
 
     if len(sys.argv) != 3:
-        print('Número de argumentos inválidos. Tente: python3 script.py exemplo.com wordlist.txt')
+        print('Numero de argumentos invalidos. Tente: python3 script.py exemplo.com wordlist.txt')
     else:
-        find_dir(url, wordlist)
+        find_dir (url, wordlist)
+
+
+#Ass: Asquen@z 
